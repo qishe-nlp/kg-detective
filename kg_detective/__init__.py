7842362513254eba9ed533ad4e6b3c0a527210b2
JingWeiLyu <murka@JingWeiLyus-MBP.powter>
2021-11-17 12:51:21 +0800

    expose kg_de_cn kg_es_cn

--- kg_detective/__init__.py
+++ kg_detective/__init__.py
@@ -1,5 +1,7 @@
-__version__ = '0.1.0'
+__version__ = '0.1.2'
 
 from .kg import KG
 from .constants import PKG_INDICES
 from .en.kg_translation import kg_en_cn
+from .es.kg_translation import kg_es_cn
+from .de.kg_translation import kg_de_cn
