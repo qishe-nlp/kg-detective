c5bc71b130d62225bbcba9a33c56f7f7602c54f8
JingWeiLyu <murka@JingWeiLyus-MBP.powter>
2024-12-31 18:51:00 +0800

    fix comma error

--- kg_detective/en/rules/verb_modal.py
+++ kg_detective/en/rules/verb_modal.py
@@ -17,7 +17,7 @@ def search_out(doc, nlp):
     i = t.i
     if i > s:
       result.append({"text": doc[s:i].text}) 
-    result.append({"text": t.text, "meta": {"sign": "verb_modal", "verb_lemma": t.lemma_ "gid": index}})
+    result.append({"text": t.text, "meta": {"sign": "verb_modal", "verb_lemma": t.lemma_, "gid": index}})
     s = i+1
   if s < len(doc):
     result.append({"text": doc[s:].text})
